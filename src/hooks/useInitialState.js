4128c9d20526cfa9a803bff223883f89f176470c
Daniel <daniel4581@protonmail.com>
2022-08-17 03:44:34 -0500

    [ADD] Last commit

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -12,11 +12,19 @@ const useInitialState = () => {
       ... state,
       cart: [... state.cart, payload]
     });
+  };
+
+  const removeFromCart = (payload) => {
+    setState({
+      ... state,
+      cart: state.cart.filter(items => items.id === payload.id),
+    });
   }
 
   return {
     state,
-    addToCart
+    addToCart,
+    removeFromCart
   };
 }
 
